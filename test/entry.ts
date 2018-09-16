298a2c63b6a7c7d716d0fbfad9652b70e0e28abc
vadim_budarin <budarin-vv@yandex.ru>
2018-09-16 23:28:11 +0300

    configuring

--- test/entry.ts
+++ test/entry.ts
@@ -1,20 +1,36 @@
-import {locals as stylesBase} from './example.css';
-import {locals as stylesCamelCase} from './example-camelcase.css';
+// @ts-ignore
+import { locals as stylesBase } from './example.css';
+// @ts-ignore
+import { locals as stylesCamelCase } from './example-camelcase.css';
+// @ts-ignore
 import * as stylesNamedExport from './example-namedexport.css';
+// @ts-ignore
 import * as stylesCamelCasedNamedExport from './example-camelcase-namedexport.css';
+// @ts-ignore
 import './example-no-css-modules.css';
+// @ts-ignore
 import * as compose from './example-compose.css';
 
 const foo = stylesBase.foo;
+console.log(foo);
+
 const barBaz = stylesBase['bar-baz'];
+console.log(barBaz);
 
 const fooCamelCase = stylesCamelCase.foo;
+console.log(fooCamelCase);
 const barBazCamelCase = stylesCamelCase.barBaz;
+console.log(barBazCamelCase);
 const barBazDashedCamelCase = stylesCamelCase['bar-baz'];
+console.log(barBazDashedCamelCase);
 
 const fooNamedExport = stylesNamedExport.foo;
+console.log(fooNamedExport);
 
 const fooCamelCaseNamedExport = stylesCamelCasedNamedExport.foo;
+console.log(fooCamelCaseNamedExport);
 const barBazCamelCaseNamedExport = stylesCamelCasedNamedExport.barBaz;
+console.log(barBazCamelCaseNamedExport);
 
 const composed = compose.test;
+console.log(composed);
--- test/webpack.config.babel.js
+++ test/webpack.config.babel.js
@@ -1,18 +1,28 @@
+const path = require('path');
+
 module.exports = {
-  entry: './test/entry.ts',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.ts$/, loaders: ['babel', 'ts'] },
-      { test: /example\.css$/, loader: '../src/index.js?modules' },
-      { test: /example-camelcase\.css$/, loader: '../src/index.js?modules&camelCase' },
-      { test: /example-namedexport\.css$/, loader: '../src/index.js?modules&namedExport' },
-      { test: /example-camelcase-namedexport\.css$/, loader: '../src/index.js?modules&camelCase&namedExport' },
-      { test: /example-no-css-modules\.css$/, loader: '../src/index.js' },
-      { test: /example-compose\.css$/, loader: '../src/index.js?modules&camelCase&namedExport' }
-    ]
-  }
+    mode: 'development',
+    entry: './test/entry.ts',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: ['ts-loader'] },
+            { test: /example\.css$/, loader: '../src/index.js?modules' },
+            { test: /example-camelcase\.css$/, loader: '../src/index.js?modules&camelCase' },
+            { test: /example-namedexport\.css$/, loader: '../src/index.js?modules&namedExport' },
+            {
+                test: /example-camelcase-namedexport\.css$/,
+                loader: '../src/index.js?modules&camelCase&namedExport',
+            },
+            { test: /example-no-css-modules\.css$/, loader: '../src/index.js' },
+            { test: /example-compose\.css$/, loader: '../src/index.js?modules&camelCase&namedExport' },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        modules: ['node_modules', './src'],
+    },
 };
