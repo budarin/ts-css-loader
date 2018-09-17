78df62548852ac534e8449e5291c3d2a440f6f57
budarin <budarin-vv@yandex.ru>
2018-09-17 03:20:05 +0300

    start refactoring

--- test/webpack.config.babel.js
+++ test/webpack.config.babel.js
@@ -10,15 +10,15 @@ module.exports = {
     module: {
         rules: [
             { test: /\.ts$/, loader: ['ts-loader'] },
-            { test: /example\.css$/, loader: '../src/index.js?modules' },
-            { test: /example-camelcase\.css$/, loader: '../src/index.js?modules&camelCase' },
-            { test: /example-namedexport\.css$/, loader: '../src/index.js?modules&namedExport' },
+            { test: /example\.css$/, loader: './src/index.js?modules' },
+            { test: /example-camelcase\.css$/, loader: './src/index.js?modules&camelCase' },
+            { test: /example-namedexport\.css$/, loader: './src/index.js?modules&namedExport' },
             {
                 test: /example-camelcase-namedexport\.css$/,
-                loader: '../src/index.js?modules&camelCase&namedExport',
+                loader: './src/index.js?modules&camelCase&namedExport',
             },
-            { test: /example-no-css-modules\.css$/, loader: '../src/index.js' },
-            { test: /example-compose\.css$/, loader: '../src/index.js?modules&camelCase&namedExport' },
+            { test: /example-no-css-modules\.css$/, loader: './src/index.js' },
+            { test: /example-compose\.css$/, loader: './src/index.js?modules&camelCase&namedExport' },
         ],
     },
     resolve: {
